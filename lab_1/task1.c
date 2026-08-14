cbcf2e6e96fc1399c76ac65a6ec2ee4994c3fdd7
Lee Hao Zhe <hlee0128@student.monash.edu>
2026-08-14 12:10:26 +0800

    tweaked prime logic and added primesLessThan function

--- lab_1/task1.c
+++ lab_1/task1.c
@@ -1,44 +1,53 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
 #include <stdbool.h>
 
 
-bool prime(int k){
-    if (k < 0) {
+bool isPrime(int k) {
+    if (k <= 1) {
         return false;
     }
 
-    if (k == 0) {
+    if (k == 2) {
         return true;
     }
 
     if (k % 2 == 0) {
         return false;
     }
-
-    int limit = sqrt(k);
-        for (int i = 3; i <= limit; i += 2) {
-            if (k % i == 0) {
-                return false;
-            }
+        
+    for (int i = 3; i <= sqrt(k); i += 2) {
+        if (k % i == 0) {
+            return false;
         }
-
-        return true;
     }
+
+    return true;
+}
+
+
+void primesLessThan(int k) {
+    if (k <= 2) {
+        return;
+    }
+
+    printf("Primes less than %d: ", k);
+
+    for (int i = 2; i < k; i++) {
+        if (isPrime(i)) {
+            printf("%d, ", i);
+        }
+    }
+}
+
 
 int main() {
     int myNum;
 
-    printf("Enter a number to check if it is prime: ");
+    printf("Enter a number find prime numbers smaller than it: ");
     scanf("%d", &myNum);
-
-    if (prime(myNum)) {
-        printf("Prime\n");
-    } else {
-        printf("Not Prime\n");
-    }
-
-    return 0;
+    primesLessThan(myNum);
 }
