bbe98e5bd087605185f51b1fc8db88da3a503fe5
loolishen <loolishen016@gmail.com>
2026-09-17 12:07:10 +0800

    changed the initialisation for mpi with thread support cuz omp

--- lab_2/task2.c
+++ lab_2/task2.c
@@ -56,8 +56,9 @@ int main(int argc, char *argv[]) {
     int k = 0;
     double start_time, end_time;
 
-    // initialise MPI environment
-    MPI_Init(&argc, &argv);
+    // initialise MPI environment with thread support
+    int provided;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
